0fb6b59f7e694190c93d59be5506fd94d627b8eb
rluciver <rluciver@aol.com>
2021-01-09 23:05:08 +0700

    Menyimpan State Auth ke Local Storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import * as React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-
 import { Provider } from "react-redux";
-import store from "./app/store";
-
 import "upkit/dist/style.min.css";
+
+import store from "./app/store";
 import Home from "./pages/Home";
+
+import { listen } from "./app/listener";
+
+React.useEffect(() => {
+  listen();
+}, []);
 
 function App() {
   return (
