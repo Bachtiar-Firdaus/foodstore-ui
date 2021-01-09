72d58e6a0c77b095cf8dfa1950bf71dd63e664ee
rluciver <rluciver@aol.com>
2021-01-10 00:18:06 +0700

    Get Data Form Register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -8,26 +8,41 @@ import {
   InputPassword,
 } from "upkit";
 
+import { useForm } from "react-hook-form";
+
 export default function Register() {
+  let { register, handleSubmit, errors, setError } = useForm();
+  const onSubmit = async (formData) => {
+    let coba = JSON.stringify(formData);
+    console.log("data form", coba);
+  };
+
   return (
     <LayoutOne size="small">
       <Card color="white">
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <FormControl>
             <InputText
               name="full_name"
               placeholder="Nama Lengkap"
               fitContainer
+              ref={register}
             />
           </FormControl>
           <FormControl>
-            <InputText name="email" placeholder="EMAIL" fitContainer />
+            <InputText
+              name="email"
+              placeholder="EMAIL"
+              fitContainer
+              ref={register}
+            />
           </FormControl>
           <FormControl>
             <InputPassword
               name="password"
               placeholder="Password"
               fitContainer
+              ref={register}
             />
           </FormControl>
           <FormControl>
@@ -35,6 +50,7 @@ export default function Register() {
               name="password_confirmation"
               placeholder="Konfirmasi Password"
               fitContainer
+              ref={register}
             />
           </FormControl>
           <Button size="large" fitContainer>
