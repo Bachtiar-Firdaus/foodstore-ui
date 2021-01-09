0b2df902a3e61a47f5b9c745270cfff93237dbc1
rluciver <rluciver@aol.com>
2021-01-09 22:52:05 +0700

    Membuat Actions Creator login & logout

---
+++ src/features/Auth/actions.js
@@ -0,0 +1,14 @@
+import { USER_LOGIN, USER_LOGOUT } from "./constants";
+
+export function userLogin(user, token) {
+  return {
+    type: USER_LOGIN,
+    user,
+    token,
+  };
+}
+export function userLogout() {
+  return {
+    type: USER_LOGOUT,
+  };
+}
--- src/features/Auth/constants.js
+++ src/features/Auth/constants.js
@@ -1,1 +1,2 @@
 export const USER_LOGIN = "features/Auth/USER_LOGIN";
+export const LOG_OUT = "features/Auth/USER_LOGOUT";
