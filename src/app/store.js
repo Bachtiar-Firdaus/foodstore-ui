24a94b1238c3c09b0d3a029c893736bb8919842e
rluciver <rluciver@aol.com>
2021-01-09 23:29:31 +0700

    Membuat API helper auth

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,11 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import authReducer from "../features/Auth/reducer";
+import thunk from "redux-thunk";
 // untuk konek ke dev tools
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
-  auth = authReducer
+  auth: authReducer,
 });
 const store = createStore(
   rootReducers,
--- src/features/Auth/reducer.js
+++ src/features/Auth/reducer.js
@@ -1,5 +1,7 @@
-import { USER_LOGIN } from "./constants";
-let initialState = { user: null, token: null };
+import { USER_LOGIN, USER_LOGOUT } from "./constants";
+let initialState = localStorage.getItem("auth")
+  ? JSON.parse(localStorage.getItem("auth"))
+  : { user: null, token: null };
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case USER_LOGIN:
