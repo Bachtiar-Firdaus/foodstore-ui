b70b5286eff3dccf0680e459e9064934c44037cb
rluciver <rluciver@aol.com>
2021-01-09 22:28:35 +0700

    Membuat Layout Awal Halaman Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,20 @@ import menus from "./menus";
 export default function Home() {
   return (
     <div>
-      <LayoutSidebar sidebar={<SideNav items={menus} verticalAlign="top" />} />
+      <LayoutSidebar
+        sidebar={<SideNav items={menus} verticalAlign="top" />}
+        content={
+          <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
+            <div className="w-full md:w-3/4 pl-5 pb-10">
+              Konten utama di sini
+            </div>
+            <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
+              Keranjang belanja di sini
+            </div>
+          </div>
+        }
+        sidebarSize={80}
+      />
     </div>
   );
 }
