ae1d83af0c8aa90a4466074c9399b2eb992f4d66
rluciver <rluciver@aol.com>
2021-01-09 22:56:19 +0700

    Mendaftarkan reducer Auth ke store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import authReducer from "../features/Auth/reducer";
 // untuk konek ke dev tools
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootReducers = combineReducers({});
+
+const rootReducers = combineReducers({
+  auth = authReducer
+});
 const store = createStore(
   rootReducers,
   composerEnhancer(applyMiddleware(thunk))
--- src/features/Auth/constants.js
+++ src/features/Auth/constants.js
@@ -1,2 +1,2 @@
 export const USER_LOGIN = "features/Auth/USER_LOGIN";
-export const LOG_OUT = "features/Auth/USER_LOGOUT";
+export const USER_LOGOUT = "features/Auth/USER_LOGOUT";
