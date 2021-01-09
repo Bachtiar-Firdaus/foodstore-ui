f9daee5d0774f90756f71201a1d2c09ae8d3f10e
rluciver <rluciver@aol.com>
2021-01-10 00:02:27 +0700

    Membuat Tampilan Form

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,47 @@
 import * as React from "react";
+import {
+  LayoutOne,
+  Card,
+  FormControl,
+  InputText,
+  Button,
+  InputPassword,
+} from "upkit";
 
 export default function Register() {
-  return <div>Hello REGISTER</div>;
+  return (
+    <LayoutOne size="small">
+      <Card color="white">
+        <form>
+          <FormControl>
+            <InputText
+              name="full_name"
+              placeholder="Nama Lengkap"
+              fitContainer
+            />
+          </FormControl>
+          <FormControl>
+            <InputText name="email" placeholder="EMAIL" fitContainer />
+          </FormControl>
+          <FormControl>
+            <InputPassword
+              name="password"
+              placeholder="Password"
+              fitContainer
+            />
+          </FormControl>
+          <FormControl>
+            <InputPassword
+              name="password_confirmation"
+              placeholder="Konfirmasi Password"
+              fitContainer
+            />
+          </FormControl>
+          <Button size="large" fitContainer>
+            Mendaftar
+          </Button>
+        </form>
+      </Card>
+    </LayoutOne>
+  );
 }
