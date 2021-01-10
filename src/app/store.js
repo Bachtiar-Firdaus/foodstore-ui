2676cc124c0c85afbde466180ffe039bfba9cd1b
rluciver <rluciver@aol.com>
2021-01-11 02:29:13 +0700

    menambahkan reducer cart ke redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import authReducer from "../features/Auth/reducer";
 import productReducer from "../features/Products/reducer";
+import cartReducer from "../features/Cart/reducer";
 import thunk from "redux-thunk";
 // untuk konek ke dev tools
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
+  cart: cartReducer,
   products: productReducer,
 });
 
