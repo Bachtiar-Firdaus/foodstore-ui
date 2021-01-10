3ff54341d9ba00171d0f37418382732556048054
rluciver <rluciver@aol.com>
2021-01-11 02:43:59 +0700

    Menyimpan Data cart ke Web API

--- src/app/listener.js
+++ src/app/listener.js
@@ -1,4 +1,5 @@
 import store from "./store";
+import { saveCart } from "../api/cart";
 
 let currentAuth;
 let currentCart;
@@ -6,13 +7,18 @@ let currentCart;
 function listener() {
   let previousAuth = currentAuth;
   let previousCart = currentCart;
+  let { token } = currentAuth;
+
   currentAuth = store.getState().auth;
   currentCart = store.getState().cart;
+  let { token } = currentAuth;
   if (currentAuth !== previousAuth) {
     localStorage.setItem("auth", JSON.stringify(currentAuth));
+    saveCart(token, currentCart);
   }
   if (currentCart !== previousCart) {
     localStorage.setItem("cart", JSON.stringify(currentCart));
+    saveCart(token, currentCart);
   }
 }
 function listen() {
