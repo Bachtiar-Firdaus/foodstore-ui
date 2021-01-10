75fa05fdc2073c2ed8c575273546449064581c4c
rluciver <rluciver@aol.com>
2021-01-10 17:12:19 +0700

    Membuat Komponen TopBar dan Menggunakan Komponen <TopBar> pada Halaman Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { SideNav, LayoutSidebar } from "upkit";
 import menus from "./menus";
+import TopBar from "../../components/TopBar";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         content={
           <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
             <div className="w-full md:w-3/4 pl-5 pb-10">
-              Konten utama di sini
+              <TopBar />
             </div>
             <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
               Keranjang belanja di sini
