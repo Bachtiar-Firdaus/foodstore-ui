8dc0ed9d0a65f54a1a35c46b460cd4cc335d8e3e
rluciver <rluciver@aol.com>
2021-01-10 17:53:29 +0700

    Action Fetching Products dari API

--- src/features/Products/actions.js
+++ src/features/Products/actions.js
@@ -1,3 +1,33 @@
+import debounce from "debounce-promise";
+import { getProducts } from "../../api/product";
+
+let debouncedFetchProducts = debounce(getProducts, 1000);
+export const fetchProducts = () => {
+  return async (dispatch, getState) => {
+    dispatch(startFetchingProducts());
+    let perPage = getState().products.perPage || 9;
+    let currentPage = getState().products.currentPage || 1;
+    let tags = getState().products.tags || [];
+    let keyword = getState().products.keyword || "";
+    let category = getState().products.category || "";
+    const params = {
+      limit: perPage,
+      skip: currentPage * perPage - perPage,
+      q: keyword,
+      tags,
+      category,
+    };
+    try {
+      let {
+        data: { data, count },
+      } = await debouncedFetchProducts({ params });
+
+      dispatch(successFetchingProducts({ data, count }));
+    } catch (err) {
+      dispatch(errorFetchingProducts());
+    }
+  };
+};
 export const startFetchingProducts = () => {
   return {
     type: START_FETCHING_PRODUCT,
