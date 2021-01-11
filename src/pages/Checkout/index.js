d7efe6cc8bbed8684d16ed6937a582ee92ef329a
rluciver <rluciver@aol.com>
2021-01-12 03:04:26 +0700

    Pembuatan (Step 2: Daftar Alamat)

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -2,14 +2,17 @@ import * as React from "react";
 import FaCartPlus from "@meronex/icons/fa/FaCartPlus";
 import FaAddressCard from "@meronex/icons/fa/FaAddressCard";
 import FaInfoCircle from "@meronex/icons/fa/FaInfoCircle";
-import { LayoutOne, Text, Steps, Table, Button } from "upkit";
+import { LayoutOne, Text, Steps, Table, Button, Responsive } from "upkit";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import TopBar from "../../components/TopBar";
 import { config } from "../../config";
 import { formatRupiah } from "../../utils/format-rupiah";
 import { sumPrice } from "../../utils/sum-price";
 import FaArrowRight from "@meronex/icons/fa/FaArrowRight";
+import FaArrowLeft from "@meronex/icons/fa/FaArrowLeft";
+import { useAddressData } from "../../hooks/address";
 
 const IconWrapper = ({ children }) => {
   return <div className="text-3xl flex justify-center">{children}</div>;
@@ -73,36 +76,109 @@ const columns = [
     },
   },
 ];
+const addressColumns = [
+  {
+    Header: "Nama alamat",
+    accessor: (alamat) => {
+      return (
+        <div>
+          {alamat.nama}
+          <br />
+          <small>
+            {alamat.provinsi},{alamat.kabupaten},{alamat.kecamatan},
+            {alamat.kelurahan}
+            <br />
+          </small>
+        </div>
+      );
+    },
+  },
+];
 export default function Checkout() {
   let [activeStep, setActiveStep] = React.useState(0);
   let cart = useSelector((state) => state.cart);
+  let { data, status, limit, page, count, setPage } = useAddressData();
+  let [selectedAddress, setSelectedAddress] = React.useState(null);
   return (
     <LayoutOne>
       <TopBar />
       <Text as="h3"> Checkout </Text>
-      <Steps steps={[steps]} active={activeStep} />
+      <Steps steps={steps} active={activeStep} />
       {activeStep === 0 ? (
         <div>
-          <br />
-          <br />
+          <br /> <br />
           <Table
             items={cart}
             columns={columns}
             perPage={cart.length}
             showPagination={false}
           />
+          {/* siap-siap untuk menampilkan informasi sub total dan tombol selanjutnya */}
           <br />
           <div className="text-right">
             <Text as="h4">Subtotal: {formatRupiah(sumPrice(cart))}</Text>
+
             <br />
             <Button
               onClick={(_) => setActiveStep(activeStep + 1)}
               color="red"
               iconAfter={<FaArrowRight />}
             >
-              Selanjutnya
+              {" "}
+              Selanjutnya{" "}
             </Button>
           </div>
+        </div>
+      ) : null}
+
+      {activeStep === 1 ? (
+        <div>
+          <br />
+          <br />
+          <Table
+            items={data}
+            columns={addressColumns}
+            perPage={limit}
+            page={page}
+            onPageChange={(page) => setPage(page)}
+            totalItems={count}
+            isLoading={status === "process"}
+            selectable
+            primaryKey={"_id"}
+            selectedRow={selectedAddress}
+            onSelectRow={(item) => setSelectedAddress(item)}
+          />
+          {!data.length && status === "success" ? (
+            <div className="text-center my-10">
+              <Link to="/alamat-pengiriman/tambah">
+                Kamu belum memiliki alamat pengiriman <br /> <br />
+                <Button> Tambah alamat </Button>
+              </Link>
+            </div>
+          ) : null}
+          <br /> <br />
+          <Responsive desktop={2} tablet={2} mobile={2}>
+            <div>
+              <Button
+                onClick={(_) => setActiveStep(activeStep - 1)}
+                color="gray"
+                iconBefore={<FaArrowLeft />}
+              >
+                Sebelumnya
+              </Button>
+            </div>
+
+            <div className="text-right">
+              <Button
+                onClick={(_) => setActiveStep(activeStep + 1)}
+                disabled={!selectedAddress}
+                color="red"
+                iconAfter={<FaArrowRight />}
+              >
+                Selanjutnya
+              </Button>
+            </div>
+          </Responsive>
         </div>
       ) : null}
     </LayoutOne>
